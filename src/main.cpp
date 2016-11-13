0fd306db889a1b67731444f746857463ca69b528
Joshua Reaves <reave006@cougars.csusm.edu>
2016-11-13 10:18:22 -0800

    added important files

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------
+CS 435 Embedded Systems Project:
+Joshua Reaves
+Ankit Panchil
+Matt
+
+This project will enable the nucleo board to play a series of notes when buttons
+are pressed.  Also, lights may flash.  Also a button will be used to play back 
+notes that were recoreded.  
+ *----------------------------------------------------------------------------*/
+
+#include "mbed.h"
+#include "pindef.h"
+
+PwmOut speaker(PB_10);
+note1 (PA_0);
+note2 (PA_1);
+note3 ();
+
+
+
+int main()
+{
+		while(1)
+		{			
+	  }
+}
+
+
